bd7cad7f4a910e7bb41889ce32e39510ac7b3d03
wootaik Lee <fined000@naver.com>
2024-02-15 14:18:37 +0900

    second

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ import {jsPDF} from 'jspdf'
 import { format } from 'date-fns'
 import cors from 'cors'
 import axios from 'axios'
+import FormData from 'form-data'
+import puppeteer from "puppeteer";
+import fs from 'fs'
+import { JSDOM } from 'jsdom'
 
 const PORT = 4000;
 const app = express();
@@ -14,6 +18,37 @@ app.use(cors())
 
 const date = new Date()
 let blobFile = null
+
+const handleHtml = async (htmlElement) => {
+  const htmlString = `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+  </head>
+  <body>
+    ${htmlElement}
+  </body>
+  </html>
+  `
+  const dom = new JSDOM(htmlString)
+  const document = dom.window.document
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setContent(document.documentElement.outerHTML);
+  await page.screenshot({ path: 'screenshot.png' });
+  await browser.close();
+}
+
+// const handleBrowser = async () => {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+//   await page.goto(`file://${__dirname}/index.html`);
+//   await page.screenshot({ path: 'screenshot.png' });
+//   await browser.close();
+// }
 
 const handleUploadFile = async (password, mstSeq) => {
   const formData = new FormData()
@@ -40,44 +75,54 @@ const handleUploadFile = async (password, mstSeq) => {
   }
 }
 
+
 app.post("/test", async function (req, res) {
   const height = req.body?.height;
-  const imgData = req.body?.imgData;
   const password = req.body?.password;
   const mstSeq = req.body?.mstSeq;
-  
-  const doc = new jsPDF({
-    orientation: 'p',
-    unit: 'mm',
-    format: 'a4',
-    encryption: {
-      userPassword: password,
-      ownerPassword: password,
-      userPermissions: ['print', 'modify', 'copy', 'annot-forms'],
-    },
-  });
-  let imgWidth = 210;
-  let pageHeight = 295
-  let imgHeight = height
-  let heightLeft = imgHeight
-  let position = 0
+  const htmlElement = req.body?.htmlElement;
 
-  doc?.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight)
-  heightLeft -= pageHeight
-  while (heightLeft >= 20) {
-    position = heightLeft - imgHeight
-    doc?.addPage()
-    doc?.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight)
-    heightLeft -= pageHeight
+  try {
+    handleHtml(htmlElement)
+    // await handleHtml(htmlElement)
+    // const canvas = await html2canvas(htmlElement, { scale: 2 })
+    // const imgData = canvas.toDataURL('image/png')
+    // const doc = new jsPDF({
+    //   orientation: 'p',
+    //   unit: 'mm',
+    //   format: 'a4',
+    //   encryption: {
+    //     userPassword: password,
+    //     ownerPassword: password,
+    //     userPermissions: ['print', 'modify', 'copy', 'annot-forms'],
+    //   },
+    // });
+    // let imgWidth = 210;
+    // let pageHeight = 295
+    // let imgHeight = height
+    // let heightLeft = imgHeight
+    // let position = 0
+  
+    // doc?.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight)
+    // heightLeft -= pageHeight
+    // while (heightLeft >= 20) {
+    //   position = heightLeft - imgHeight
+    //   doc?.addPage()
+    //   doc?.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight)
+    //   heightLeft -= pageHeight
+    // }
+    // const file = doc?.output('blob')
+    // let data = new File([file], `best_${format(date, 'yyyyMMddHHmmss')}.pdf`, { type: 'application/pdf' })
+    // blobFile = data
+  
+    // let blobUrl = URL.createObjectURL(data)
+    // await handleUploadFile(password, mstSeq)
+    // res.send(blobUrl)
+  } catch (error) {
+    console.log(error)
   }
+});
 
-  const file = doc?.output('blob')
-  let data = new File([file], `best_${format(date, 'yyyyMMddHHmmss')}.pdf`, { type: 'application/pdf' })
-  blobFile = data
-  
-  handleUploadFile(password, mstSeq)
-  res.send('success')
-  });
 const handleListening = () => {
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 }
